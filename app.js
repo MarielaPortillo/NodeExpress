f660fcc53a638d7dfc4bf0ad8e2f53ff55caf6f4
Lawliet <49668391+alramirez97@users.noreply.github.com>
2021-09-29 16:39:05 -0600

    Documentacion al 100 everyday everynight bien bien hight XD

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-
 import  Express  from "express";
 import Morgan from "morgan";
 import RutasComercio from "./routes/comercio.routes.js";
 import RutasUsuario from   "./routes/usuario.routes.js";
 import RutasAuth from   "./routes/autenticar.routes.js";
-import swaggerUi from 'swagger-ui-express';
+import cors from "cors";
+import swaggerUI from "swagger-ui-express";
 import swaggerJSDoc from "swagger-jsdoc";
 import { createRoles, createAdmin, createCategoria } from "./libs/configInit.js";
 
@@ -13,27 +13,29 @@ createAdmin();
 createCategoria();
 
 
-const options =  {
-    swaggerDefinition: {
-        info: {
-            version: "1.0.0",
-            title: "Documentacion API",
-            description: "Documentando la API",
-            contact: {
-            name: "Belmont"
-            },
-            servers: ["http://localhost:3000"]
-        }
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "API de comercios",
+      version: "1.0.0",
+      description:
+        "Esta es una API para poder encontrar informacion de los comercios",
     },
-    // ['.routes/*.js']
-    apis: ["./routes/*.js"]
-   
-    
-  };
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
 const app = Express();
 app.use(Express.json());
 app.use(Express.urlencoded({extended:true})); 
 app.use(Morgan('dev'));
+app.use(cors());
+
 app.use((req, res, next) => {
         res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method', 'x-access-token');
@@ -48,8 +50,8 @@ app.set('puerto', process.env.PORT || 3000)
 
 
 
-const swaggerDocs = swaggerJSDoc(options);
-app.use('/api-documentacion', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+const specs = swaggerJSDoc(options);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
 
 
